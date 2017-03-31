2c3f7ddca72ccd0e6e440f1426940111985a1d62
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-03-30 20:36:18 -0400

    Updating testing files

--- Team20MSD/src/test/java/TestQueryEngine.java
+++ Team20MSD/src/test/java/TestQueryEngine.java
@@ -1,7 +1,31 @@
 package test.java;
 
-import junit.framework.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
 
-public class TestQueryEngine extends TestCase {
+import main.java.classes.Author;
+
+public class TestQueryEngine {
+	
+	@Test
+	public void testHibernate() {
+		SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+ 
+		Author user = new Author("testAuthor", null);
+		session.save(user);
+ 
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	@Test
+	public void testSearchAuthorQuery() {
+		
+	}
 
 }
