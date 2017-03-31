32f8800f847bb9014be4d5026b5173f5b1263207
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-03-31 10:44:05 -0400

    Updated the query engine test case

--- Team20MSD/src/test/java/TestQueryEngine.java
+++ Team20MSD/src/test/java/TestQueryEngine.java
@@ -1,9 +1,21 @@
 package test.java;
 
+import java.util.List;
+
+import org.junit.Test;
+
+import main.java.classes.Author;
+import main.java.classes.SearchQuery;
+
 public class TestQueryEngine {
 	
-	public void testSearchAuthorQuery() {
+	@Test
+	public void testPopulateListOfAuthorsQuery() {
+		SearchQuery searchQuery = new SearchQuery();
 		
+		int[] intArray = new int[1];
+		intArray[0] = 1996;
+		List<Author> resultList = searchQuery.populateListOfAuthors("acta inf.", "parallel", intArray, 2);
 	}
 
 }
