a8745ceb668373ddea73410283bd3a522c705088
Dhara Bhavsar <dharapb@ccs.neu.edu>
2017-04-16 02:14:50 -0400

    adding few input validations

--- phase4/Team20MSD/src/main/search/Result.java
+++ phase4/Team20MSD/src/main/search/Result.java
@@ -21,38 +21,32 @@ public class Result {
 
 	private TableView table = new TableView();
 
-    ObservableList<Author> products = FXCollections.observableArrayList();
+	ObservableList<Author> products = FXCollections.observableArrayList();
 
-	public void setCurrentInfo(String key,
-							   String conf,
-							   String date,
-							   String numofpub,
-							   String commit,
-							   boolean isSearvedAsCommittee,
-							   UserInterface ui,
-							   QueryEngine qe) {
-		
-		// TODO : if multiple year selection enabled - change code
-		 int[] intArray = new int[1];
-		 intArray[0] = Integer.parseInt(date);
-		 List<Author> authorList = qe.populateListOfAuthors(
-		 		conf.trim(),
-				 key.trim(),
-				 intArray,
-				 Integer.parseInt(numofpub),
-				 isSearvedAsCommittee);
-		 
-		 for (Author a : authorList) {
-			 products.add(new Author(a.getTitle(), a.getName(), a.getNoOfPublication()));
-		 }
-		 
-		 this.ui = ui;
-		 this.qe = qe;
+	public void setCurrentInfo(String key, String conf, String date, String numofpub, String commit,
+			boolean isSearvedAsCommittee, UserInterface ui, QueryEngine qe) {
+
+		int[] intArray = null;
+		if (date != null) {
+			// TODO : if multiple year selection enabled - change code
+			intArray = new int[1];
+			intArray[0] = Integer.parseInt(date);
+		}
+
+		List<Author> authorList = qe.populateListOfAuthors(conf, key, intArray, Integer.parseInt(numofpub),
+				isSearvedAsCommittee);
+
+		for (Author a : authorList) {
+			products.add(new Author(a.getTitle(), a.getName(), a.getNoOfPublication()));
+		}
+
+		this.ui = ui;
+		this.qe = qe;
 	}
 
 	public Scene getScene() {
 
-		//window.setTitle("Result Page");
+		// window.setTitle("Result Page");
 
 		// Name column
 		TableColumn<Author, String> nameColumn = new TableColumn<>("Author_Name");
@@ -87,23 +81,23 @@ public class Result {
 		return new Scene(vBox);
 	}
 
-//	public ObservableList<Author> getProduct() {
-//		ObservableList<Author> products = FXCollections.observableArrayList();
-//		products.add(new Author("Laptop", "Laptop", "Laptop"));
-//		products.add(new Author("Laptop1", "Laptop1", "Laptop1"));
-//		products.add(new Author("Laptop2", "Sanjeev Saxena", "Laptop2"));
-//		products.add(new Author("Laptop3", "Laptop3", "Laptop3"));
-//		return products;
-//	}
+	// public ObservableList<Author> getProduct() {
+	// ObservableList<Author> products = FXCollections.observableArrayList();
+	// products.add(new Author("Laptop", "Laptop", "Laptop"));
+	// products.add(new Author("Laptop1", "Laptop1", "Laptop1"));
+	// products.add(new Author("Laptop2", "Sanjeev Saxena", "Laptop2"));
+	// products.add(new Author("Laptop3", "Laptop3", "Laptop3"));
+	// return products;
+	// }
 
 	// Delete button clicked
 	public void ButtonClicked() {
 		ObservableList<Author> AuthorSelected;
 		AuthorSelected = table.getSelectionModel().getSelectedItems();
-		AuthorProfilePage page = new AuthorProfilePage(AuthorSelected.get(0),ui);
+		AuthorProfilePage page = new AuthorProfilePage(AuthorSelected.get(0), ui);
 		ui.displayNewWindow(page.getScene());
 	}
-	
+
 	private UserInterface ui;
 	private QueryEngine qe;
 }
--- phase4/Team20MSD/src/main/search/searchcontroller.java
+++ phase4/Team20MSD/src/main/search/searchcontroller.java
@@ -48,28 +48,37 @@ public class searchcontroller implements Initializable {
 	@FXML
 	private void setSearch(Event event) throws Exception {
 		// get the criteria parameters from the search page
-		
+
 		String conferencename1 = null;
-		
-		if ( conferencename.getSelectionModel().isEmpty() ) {
-			conferencename1 = conferencename.getSelectionModel().getSelectedItem().toString().equals("") 
-					? "" : conferencename.getSelectionModel().getSelectedItem().toString();
+		// System.out.println("isNotSelected0... " +
+		// !conferencename.getSelectionModel().isSelected(0));
+		// System.out.println("isNotEmpty... " +
+		// !conferencename.getSelectionModel().isEmpty());
+		if (!conferencename.getSelectionModel().isSelected(0) && !conferencename.getSelectionModel().isSelected(-1)) {
+			// System.out.println("index... " +
+			// conferencename.getSelectionModel().getSelectedIndex());
+			conferencename1 = (conferencename.getSelectionModel().getSelectedItem().toString().equals("")
+					|| conferencename.getSelectionModel().getSelectedItem().toString().equals(" ")) ? ""
+							: conferencename.getSelectionModel().getSelectedItem().toString().trim();
 		}
-		
+
 		String pubdate1 = null;
-		if ( pubdate.getSelectionModel().isEmpty() ) {
-			pubdate1 =  pubdate.getSelectionModel().getSelectedItem().toString().equals("") 
-					? "0" : pubdate.getSelectionModel().getSelectedItem().toString();
+		// System.out.println("..isNotEmpty... " +
+		// !pubdate.getSelectionModel().isEmpty());
+		if (!pubdate.getSelectionModel().isEmpty()) {
+			pubdate1 = (pubdate.getSelectionModel().getSelectedItem().toString().equals("")
+					|| pubdate.getSelectionModel().getSelectedItem().toString().equals(" ")) ? ""
+							: pubdate.getSelectionModel().getSelectedItem().toString().trim();
 		}
-		
+
 		String numofcom1 = null;
-		if ( numofcom.getSelectionModel().isEmpty() ) {
-			numofcom1 =  numofcom.getSelectionModel().getSelectedItem().toString().equals("") 
-					? "0" : numofcom.getSelectionModel().getSelectedItem().toString();
+		if (!numofcom.getSelectionModel().isEmpty()) {
+			numofcom1 = numofcom.getSelectionModel().getSelectedItem().toString().equals("") ? "0"
+					: numofcom.getSelectionModel().getSelectedItem().toString().trim();
 		}
-		
+
 		String keyword1 = keyword.getText().equals("") ? "" : keyword.getText();
-		
+
 		String numofpub1 = numofpub.getText().equals("") ? "0" : numofpub.getText();
 
 		// int[] intArray = new int[1];
@@ -84,22 +93,22 @@ public class searchcontroller implements Initializable {
 		// pass the criteria variables to query engine
 		// TODO : remove false
 		tableViewSample.setCurrentInfo(keyword1, conferencename1, pubdate1, numofpub1, numofcom1, false, ui, qe);
-		
-		ui.displayNewWindow(tableViewSample.getScene(),"Result Page");
+
+		ui.displayNewWindow(tableViewSample.getScene(), "Result Page");
 	}
-	
+
 	@FXML
-	private void setAllclear(){
+	private void setAllclear() {
 		keyword.clear();
 		numofpub.clear();
 		conferencename.getSelectionModel().clearSelection();
 		numofcom.getSelectionModel().selectFirst();
 		pubdate.getSelectionModel().clearSelection();
 	}
-	
+
 	@FXML
 	private void searchclear(Event event) throws Exception {
-	   setAllclear();
+		setAllclear();
 	}
 
 	@FXML
@@ -112,7 +121,7 @@ public class searchcontroller implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		qe = new QueryEngine();
 		ui = new _userInterface(qe);
-		
+
 		List<String> list = qe.fetchJournalNames();
 		List<Integer> plist = qe.fetchYearsAvailable();
 		ObservableList confList = (ObservableList) FXCollections.observableList(list);
@@ -123,10 +132,10 @@ public class searchcontroller implements Initializable {
 
 		pubdate.getItems().clear();
 		pubdate.setItems(pubdateList);
-		
+
 		System.out.println("Dynamic Loading successful..");
 	}
-	
+
 	private UserInterface ui;
 	private QueryEngine qe;
 
